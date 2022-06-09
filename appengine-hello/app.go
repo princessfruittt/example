9be9a94509b77c80d7ee27194da1258b162bab3f
princessfruittt <princessfruittt@yandex.ru>
2022-06-09 13:27:45 +0000

    Revert "bad commit chain 3"
    This reverts commit 67bcf6e14fd64031884d7a94a7ea0de92d70e530.

--- appengine-hello/app.go
+++ appengine-hello/app.go
@@ -24,7 +24,7 @@ import (
 // init is run before the application starts serving.
 func init() {
 	// Handle all requests with path /hello with the helloHandler function.
-	http.HandleFunc("/hevjlbllo", helloHn/ljn[-andler)
+	http.HandleFunc("/hello", helloHandler)
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
